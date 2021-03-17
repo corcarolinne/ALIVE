2d03325c985a5e69fb3827c3fdcd47fbb862bd6c
corcarolinne <carolinnemcordeiro@gmail.com>
2021-03-17 15:37:13 +0000

    Finish all Modal fields

--- src/app/containers/RegisterModal.jsx
+++ src/app/containers/RegisterModal.jsx
@@ -1,7 +1,7 @@
 import React, { memo, useState } from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { Button } from '@material-ui/core'
+import { Button, TextField, makeStyles, FormControlLabel, Checkbox } from '@material-ui/core'
 
 import AliveModal from '../components/AliveModal'
 
@@ -15,13 +15,34 @@ const RegisterModal = () => {
   const handleClose = () => {
     setOpen(false)
   }
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }))
+
+  const classes = useStyles()
   return (
     <>
       <Button variant="contained" color="secondary" style={{ margin: '0 5px' }} onClick={handleOpen}>
         Join Now
       </Button>
-      <AliveModal open={open} onClose={handleClose} title="Get started">
-        Se divirta!
+      <AliveModal open={open} onClose={handleClose} title="Get started" color="primary">
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="email-input" label="Email" variant="filled" />
+          <TextField id="username-input" label="Username" variant="filled" />
+          <TextField id="password-input" label="Password" type="password" variant="filled" />
+          <Button variant="contained" color="secondary" style={{ margin: '0 5px' }}>
+            Sign Up
+          </Button>
+          <FormControlLabel
+            control={<Checkbox name="checkedTerms" />}
+            label="I am at least 16 years old and I accept the Terms of Use."
+          />
+        </form>
       </AliveModal>
     </>
   )
